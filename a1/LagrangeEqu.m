c887544482cd9a16e12c9bb5e1007371727a3012
Mike Shi <dongming.shi@uqconnect.edu.au>
2024-08-07 20:54:44 +1000

    updated area equ for a1q1a

--- a1/LagrangeEqu.m
+++ a1/LagrangeEqu.m
@@ -8,21 +8,12 @@ function equ = LagrangeEqu(x)
     x4 = x(4);  % lambda
 
     % equation 1: df/dx = lambda * dg/dx
-%     equ(1) = x4 - ...
-%         (x1^2*(x2 + x3) - x1*(x2^2 + 2*x2*x3 + x3^2) + x2*x3^2 + x2^2*x3 + x1^3 - x2^3 - x3^3) / ...
-%         (4*x2*x3 + 2*x1^2 + 2*x2^2 + 2*x3^2 + x1*(4*x2 + 4*x3));
     equ(1) = x4 + 4*x1*x2^2 + 4*x1*x3^2 - 4*x1^3;
         
     % equation 2: df/dy = lambda * dg/dy
-%     equ(2) = x4 + ...
-%         (x2*x3^2 - x1*(x2^2 - 2*x2*x3 + x3^2) - x2^2*x3 + x1^3 - x2^3 + x3^3 + x1^2*(x2 - x3)) / ...
-%         (4*x2*x3 + 2*x1^2 + 2*x2^2 + 2*x3^2 + x1*(4*x2 + 4*x3));
     equ(2) = x4 + 4*x1^2*x2 + 4*x2*x3^2 - 4*x2^3;
         
     % equation 3: df/dz = lambda * dg/dz
-%     equ(3) = x4 - ...
-%         (x1*(x2^2 - 2*x2*x3 + x3^2) + x2*x3^2 - x2^2*x3 - x1^3 - x2^3 + x3^3 + x1^2*(x2 - x3)) / ...
-%         (4*x2*x3 + 2*x1^2 + 2*x2^2 + 2*x3^2 + x1*(4*x2 + 4*x3));
     equ(3) = x4 + 4*x1^2*x3 + 4*x2^2*x3 - 4*x3^3;
         
     % equation 4: constraint function
--- a1/a1q1a.m
+++ a1/a1q1a.m
@@ -10,38 +10,33 @@ syms x [1 4];
 g = x1 + x2 + x3 - 100;
 
 % find the partial differential equations for the constraint functions
-gfx = diff(g, x1);    % "dg/dx"
-gfy = diff(g, x2);    % "dg/dy"
-gfz = diff(g, x3);    % "dg/dz"
+fprintf("\nConstraint Function PDE's:\n");
+gfx = diff(g, x1);  fprintf("dg/dx: %s\n", gfx);    % "dg/dx"
+gfy = diff(g, x2);  fprintf("dg/dx: %s\n", gfy);    % "dg/dy"
+gfz = diff(g, x3);  fprintf("dg/dx: %s\n", gfz);    % "dg/dz"
 
 % define object function
-% "r^2 = ((s-x)(s-y)(s-z))/s" where r is radius and s is semiperimeter
-% "s = 0.5*(x + y + z)" -> semiperimeter in terms of x, y, z
-%s = 0.5*(x1 + x2 + x3);
-%f = ((s-x1)*(s-x2)*(s-x3))/s;
-f = 2*x1^2*x2^2 + 2*x1^2*x3^2 + 2*x2^2*x3^2 - x1^4 - x2^4 - x3^4;
+% "16*A^2 = 4*(x^2)*(y^2) - (x^2 + y^2 - z^2)^2" where A is area
+f = 4*(x1^2)*(x2^2) - (x1^2 + x2^2 - x3^2)^2;
+f = expand(f);
 
 % find the partial differential equations for the object functions
-dfx = diff(f, x1);    % "df/dx"
-dfy = diff(f, x2);    % "df/dy"
-dfz = diff(f, x3);    % "df/dz"
+fprintf("\nObject Function PDE's:\n");
+dfx = diff(f, x1);  fprintf("df/dx: %s\n", dfx);    % "df/dx"
+dfy = diff(f, x2);  fprintf("df/dy: %s\n", dfy);    % "df/dy"
+dfz = diff(f, x3);  fprintf("df/dz: %s\n", dfz);    % "df/dz"
 
 % apply the lagrange equality "df = lambda*dg"
-equ1 = dfx + x4*gfx == 0; fprintf("equ1: %s\n", equ1);      % df/dx = lambda * dg/dx 
-equ2 = dfy + x4*gfy == 0; fprintf("equ2: %s\n", equ2);      % df/dy = lambda * dg/dy
-equ3 = dfz + x4*gfz == 0; fprintf("equ3: %s\n", equ3);      % df/dz = lambda * dg/dz
+fprintf("\nLagrange Equations:\n");
+equ1 = dfx + x4*gfx == 0;   fprintf("equ1: %s\n", equ1);    % df/dx = lambda * dg/dx 
+equ2 = dfy + x4*gfy == 0;   fprintf("equ2: %s\n", equ2);    % df/dy = lambda * dg/dy
+equ3 = dfz + x4*gfz == 0;   fprintf("equ3: %s\n", equ3);    % df/dz = lambda * dg/dz
 equ4 = x1 + x2 + x3 == 100; fprintf("equ4: %s\n", equ4);    % constraint function
 
 % equations output:
-% "equ1: x4 - ...
-%   (x1^2*(x2 + x3) - x1*(2*x2*x3 + x2^2 + x3^2) + x2*x3^2 + x2^2*x3 + x1^3 - x2^3 - x3^3) / ...
-%   (4*x2*x3 + 2*x1^2 + 2*x2^2 + 2*x3^2 + x1*(4*x2 + 4*x3)) == 0"
-% "equ2: x4 + ...
-%   (x2*x3^2 - x1*(x2^2 - 2*x2*x3 + x3^2) - x2^2*x3 + x1^3 - x2^3 + x3^3 + x1^2*(x2 - x3)) / ...
-%   (4*x2*x3 + 2*x1^2 + 2*x2^2 + 2*x3^2 + x1*(4*x2 + 4*x3)) == 0"
-% "equ3: x4 - ...
-%   (x1*(x2^2 - 2*x2*x3 + x3^2) + x2*x3^2 - x2^2*x3 - x1^3 - x2^3 + x3^3 + x1^2*(x2 - x3)) / ...
-%   (4*x2*x3 + 2*x1^2 + 2*x2^2 + 2*x3^2 + x1*(4*x2 + 4*x3)) == 0"
+% "equ1: x4 + 4*x1*x2^2 + 4*x1*x3^2 - 4*x1^3 == 0"
+% "equ2: x4 + 4*x1^2*x2 + 4*x2*x3^2 - 4*x2^3 == 0"
+% "equ3: x4 + 4*x1^2*x3 + 4*x2^2*x3 - 4*x3^3 == 0"
 % "equ4: x1 + x2 + x3 == 100"
 
 % solve the system of non-linear equations
@@ -49,9 +44,10 @@ equ = @LagrangeEqu;     % initialise function that defines the set of lagrange e
 x0 = [1, 1, 1, 1];      % set initial guesses to 1 (avoids divide by 0 error)
 options = optimoptions('fmincon', "MaxIterations", 1000);
 sol = fsolve(equ, x0, options);  % calls "fsolve" to solve the system of equations
+fprintf("\nResults:\n");
 fprintf("%s\n", sol);
 
-% solutions output:
+% solutions:
 % x1 -> "x = 33.3333"
 % x2 -> "y = 33.3333"
 % x3 -> "z = 33.3333"
