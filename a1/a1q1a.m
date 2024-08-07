83dbad6d08c464320cee9f75cc6851a9381f919b
Mike Shi <dongming.shi@uqconnect.edu.au>
2024-08-07 17:18:22 +1000

    updated obj function to area in a1

--- a1/a1q1a.m
+++ a1/a1q1a.m
@@ -10,20 +10,21 @@ syms x [1 4];
 g = x1 + x2 + x3 - 100;
 
 % find the partial differential equations for the constraint functions
-gfx = combine(collect(diff(g, x1)));    % "dg/dx"
-gfy = combine(collect(diff(g, x2)));    % "dg/dy"
-gfz = combine(collect(diff(g, x3)));    % "dg/dz"
+gfx = diff(g, x1);    % "dg/dx"
+gfy = diff(g, x2);    % "dg/dy"
+gfz = diff(g, x3);    % "dg/dz"
 
 % define object function
 % "r^2 = ((s-x)(s-y)(s-z))/s" where r is radius and s is semiperimeter
 % "s = 0.5*(x + y + z)" -> semiperimeter in terms of x, y, z
-s = 0.5*(x1 + x2 + x3);
-f = ((s-x1)*(s-x2)*(s-x3))/s;
+%s = 0.5*(x1 + x2 + x3);
+%f = ((s-x1)*(s-x2)*(s-x3))/s;
+f = 2*x1^2*x2^2 + 2*x1^2*x3^2 + 2*x2^2*x3^2 - x1^4 - x2^4 - x3^4;
 
 % find the partial differential equations for the object functions
-dfx = combine(collect(diff(f, x1)));    % "df/dx"
-dfy = combine(collect(diff(f, x2)));    % "df/dy"
-dfz = combine(collect(diff(f, x3)));    % "df/dz"
+dfx = diff(f, x1);    % "df/dx"
+dfy = diff(f, x2);    % "df/dy"
+dfz = diff(f, x3);    % "df/dz"
 
 % apply the lagrange equality "df = lambda*dg"
 equ1 = dfx + x4*gfx == 0; fprintf("equ1: %s\n", equ1);      % df/dx = lambda * dg/dx 
@@ -46,11 +47,11 @@ equ4 = x1 + x2 + x3 == 100; fprintf("equ4: %s\n", equ4);    % constraint functio
 % solve the system of non-linear equations
 equ = @LagrangeEqu;     % initialise function that defines the set of lagrange equations
 x0 = [1, 1, 1, 1];      % set initial guesses to 1 (avoids divide by 0 error)
-sol = fsolve(equ, x0);  % calls "fsolve" to solve the system of equations
-disp(sol);
+options = optimoptions('fmincon', "MaxIterations", 1000);
+sol = fsolve(equ, x0, options);  % calls "fsolve" to solve the system of equations
+fprintf("%s\n", sol);
 
 % solutions output:
 % x1 -> "x = 33.3333"
 % x2 -> "y = 33.3333"
 % x3 -> "z = 33.3333"
-% x4 -> "lambda = -1.8519"
